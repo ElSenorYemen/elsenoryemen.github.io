8cc4dda1e1a3871bd54284417964d2571374965c
ElSenorYemen <kaidzak@gmail.com>
2021-05-26 14:20:05 -0400

    Made changes to app.js (now have errors but thats okay)

--- src/App.js
+++ src/App.js
@@ -1,33 +1,35 @@
 
 import logo from './Zak.JPG';
+import React from 'react';
+import './App.css';
+import { BrowserRouter as Router, Route,} from "react-router-dom";
+import navbar from './compentents/navbar';
+import About from './Components/about';
+
+
 
 function App() {
   return (
+    <Router>
     <div className="App">
-      <header className="App-header" >
-        <div className="center-image">
-        <img src={logo} className="App-logo" alt="logo"
-        width="650"
-        height="400" 
-        />
-        </div>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-
-         <p>My name is Zak and I am going to master React</p> 
+      <navbar />
+      <Route exact path="/">
+        <Home />
+      </Route>
+      <Route path="/about">
+        <About />
+      </Route>
+      <Route path="/contact">
+        <Contact />
+      </Route>
         
-        </p>
-        <a
-          className="App-link"
-          href="https://github.com/ElSenorYemen"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Check me out fam!
-        </a>
-      </header>
+
+
+
     </div>
+    </Router>
   );
 }
 
 export default App;
+
