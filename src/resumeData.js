db6912cb569eb3e2be4e9224e59cccb9ae549850
ElSenorYemen <kaidzak@gmail.com>
2021-05-26 11:52:15 -0400

    Created resumedata file and added some data

---
+++ src/resumeData.js
@@ -0,0 +1,120 @@
+let resumeData = {
+    "imagebaseurl":"https://elsenoryemen.github.io/",
+    "name": "Zak Kaid",
+    "role": "Front-End Developer",
+    "linkedinId":" @zakkaid",
+    "roleDescription": " I love to learn about all the various aspects and forms of technology, as well as it's growth in the tech field. On a daily basis, I love to challenge myself to become the best version of myself, which includes perfecting my craft in the computer world.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/zak-kaid/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"http://github.com/Elsenoryemen",
+          "className":"fa fa-github"
+        }
+      ],
+    "aboutme":"I am currently a Senior at Lehman College and pursuing a BS In Computer Science. I am a aspiring Full Stack Web Developer, currently diving deeper into Software Development. In order for an individual to achieve his or her goals, one must be focused, determined and extremely hardworking. You have to want it more than anyone else!!",
+    "address":"New York, New York",
+    "website":"https://Elsenoryemen.github.io",
+    "education":[
+      {
+        "UniversityName":"Lehman College",
+        "specialization":"Senior College of CUNY",
+        "MonthOfPassing":"May",
+        "YearOfPassing":"2021",
+        "Achievements":"Pursuing Bachelors of Science in Computer Science"
+      },
+      {
+        "UniversityName":"Wood-Tobe Coburn",
+        "specialization":"1st year of College",
+        "MonthOfPassing":"Dec",
+        "YearOfPassing":"2015",
+        "Achievements":"Attendance"
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"Tech Headz",
+        "specialization":"IT Consultant",
+        "MonthOfLeaving":"December",
+        "YearOfLeaving":"2016",
+        "Achievements":"Employee of the Month"
+      },
+      {
+        "CompanyName":"The Imaginary Company",
+        "specialization":"Imagination Expert",
+        "MonthOfLeaving":"June",
+        "YearOfLeaving":"2021",
+        "Achievements":"Genius of the Century"
+      }
+    ],
+    "skillsDescription":"Your skills here",
+    "skills":[
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"JAVASCIPT"
+      },
+      {
+        "skillname":"Reactjs"
+      },
+      {
+        "skillname":"Java"  
+      },
+      {
+        "skillname":"Swift"  
+      },
+      {
+        "skillname":"mySQL"  
+      },
+      {
+        "skillname":"python"  
+      },
+      {
+        "skillname":""  
+      }
+
+      
+    ],
+    "portfolio":[
+      {
+        "name":"Project 1",
+        "description":"application",
+        "imgurl":""
+      },
+      {
+        "name":"Project 2",
+        "description":"application",
+        "imgurl":""
+      },
+      {
+        "name":"Project 3",
+        "description":"application",  
+        "imgurl":""
+      },
+      {
+        "name":"Project 4",
+        "description":"application",
+        "imgurl":""
+      }
+    ],
+    "testimonials":[
+      {
+        "description":"This is a sample feedback",
+        "name":"Just some random Engineer"
+      },
+      {
+        "description":"This is a sample feedback",
+        "name":"Just some random Engineer"
+      }
+    ]
+  }
+  
+  export default resumeData
